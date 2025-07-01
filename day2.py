8f178097e0793a73b4bf21a3ae27fd307a9c40ed
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-01 06:41:00 +0530

    day2 pushed

---
+++ day2.py
@@ -0,0 +1,37 @@
+# isEligible = False
+
+# print(type(isEligible))
+
+# myData = {
+#     "name":"Nandini",
+#     "age":18,
+#     "city":"Hyderabad"
+# }
+
+# print(type(myData))
+
+# a = {1,1,1,2,3,2,2,3,4,1,1}
+# print(type(a))
+
+# name = '''I'm doing "Good"'''
+# print((name))
+
+
+# a = (1.2,1,2,True,"Nandini")
+
+# print(type(a))
+
+
+num = 123
+
+print(type(num))
+
+num = str(num)
+
+print(type(num))
+
+
+a = 10
+b =20
+print("The addition of",a ,"and",b,"is ",a+b)
+print(f"The addition of {a} and {b} is {a+b}")
