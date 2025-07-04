31dfbbcfe78da0b0e50032789d6e419924aa01d5
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-04 06:54:03 +0530

    day3 pushed

---
+++ day3.py
@@ -0,0 +1,52 @@
+# name = "NanDini sUnkaRa"
+
+# print("uday".capitalize())
+# ans = name.capitalize()
+# print(ans)
+
+# ans = name.casefold()
+# print(ans)
+
+# ans = len(name)
+
+# print(ans)
+
+
+# ans = name.center(16,"#")
+# print(ans)
+
+# ans = name.count("ara")
+# print(ans)
+
+
+# ans = name.startswith("Nandini")
+# print(ans)
+
+
+# ans = name.endswith("a")
+# print(ans)
+
+# "uday.valapudau@theksquaregroup.com".endswith("theksquaregroup.com")
+
+
+# ans = name.find("uday")
+# print(ans)
+
+# ans = name.index("uday")
+# print(ans)
+
+# ans = name.lower()
+# print(ans)
+
+# ans = name.upper()
+# print(ans)
+
+
+# ans = name.swapcase()
+# print(ans)
+
+
+
+
+
+
