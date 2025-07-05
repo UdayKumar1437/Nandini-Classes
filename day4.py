05be3f63403a05996d801b7f08060cc242d5a20f
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-05 06:33:20 +0530

    day 4 pushed

---
+++ day4.py
@@ -0,0 +1,68 @@
+# a = "nandini123"
+
+# ans = a.isalnum()
+
+
+# if a.isalnum():
+#     print("Proceed")
+# else:
+#     print("error")
+
+# print(ans)
+
+
+# a = "nandini"
+# ans = a.isalpha()
+# print(ans)
+
+# a = "uday"
+
+# ans = a.isascii()
+
+# print(ans)
+
+# a = "456"
+# ans = a.isdecimal()
+# print(ans)
+
+
+# a = "456²"
+# ans = a.isdigit()
+# print(ans)
+
+
+# a = "uday"
+
+# ans = a.islower()
+# print(ans)
+
+
+# a = "UDAy"
+# ans = a.isupper()
+# print(ans)
+
+
+# a = "  s "
+# ans = a.isspace()
+# print(ans)
+
+
+# a = "Nandini Sunkara"
+# ans = a.istitle()
+# print(ans)
+
+
+# a = "nandini sunkara"
+# ans = a.title()
+# print(ans.istitle())
+# print(ans)
+
+
+# a = "nandini is a good girl and is a good and is"
+# ans = a.replace("good","bad")
+# print(ans)
+
+
+a = "NanDiNi SuNKAra"
+ans = a.swapcase()
+print(ans,a)
