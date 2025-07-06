ce0a9b2ad6ce82a3eefd2aaa121faa89608b5735
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-06 06:38:54 +0530

    day5 pushed

---
+++ day5.py
@@ -0,0 +1,35 @@
+# for i in range(1,6,1):
+#     if "toy" in i:
+#         print(i)
+
+
+# for i in range(1,3,1):
+#     print(i*5)
+
+
+# for i in range(10,15,2):
+#     print(i)
+
+
+# for i in range(1,22,3):
+#     if i%2 ==0:
+#         print(i)
+
+
+# for i in range(1,101,1):
+#     if i%3 ==0:
+#         print(i)
+
+
+# for i in range(2,10):
+#     print(i)
+
+# num = 14
+# for i in range(1,num+1):
+#     if num%i == 0:
+#         print(i) 
+
+# for i in range(0,50001,2):
+#     print(i)
+
+
