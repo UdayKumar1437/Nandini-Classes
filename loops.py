a1eb6173c96e844eb72400171f9a7ae267fb8a30
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-11 06:44:17 +0530

    programs pushed

--- loops.py
+++ loops.py
@@ -53,12 +53,84 @@
 #     print("Not an Armstrong number")
 
 
-num = 12
-sum = 0
-for i in range(1,num):
-    if num%i ==0:
-        sum +=i
-if sum>num:
-    print("Abudant Number")
-else:
-    print("Not an Abudant Number")
+# num = 12
+# sum = 0
+# for i in range(1,num):
+#     if num%i ==0:
+#         sum +=i
+# if sum>num:
+#     print("Abudant Number")
+# else:
+#     print("Not an Abudant Number")
+
+
+# print(ord(" "))
+
+# name = "nanDiNi"
+# ans = ""
+# for i in name:
+#     if i.isupper():
+#         ans+=i.lower()
+#     elif i.islower():
+#         ans+=i.upper()
+#     else:
+#         ans+=i
+# print(ans)
+
+
+# name = "nandini"
+# ans = ""
+# for i in name:
+#     if i not in "aeiouAEIOU":
+#         ans +=i
+# print(ans)
+
+# name = "Nand23ini@!"
+# ans = ""
+# for i in name:
+#     if i.isalpha():
+#         ans+=i
+# print(ans)
+
+# name = "Nandi ni"
+# ans = ""
+# for i in name:
+#     if ord(i) != 32:
+#         ans+=i
+# print(ans)
+
+
+# a = "Nan123dini6"
+# ans = 0
+# for i in a:
+#     if i.isdigit():
+#         ans+=int(i)
+# print(ans)
+
+
+# num = 21
+# sum = 0
+# for i in str(num):
+#     sum+=int(i)
+# if num%sum == 0:
+#     print("harshad")
+# else:
+#     print('Not a Harshad')
+
+# num = 10
+# for i in range(1,num+1):
+#     if num%i == 0:
+#         count = 0
+#         for j in range(1,i+1):
+#             if i%j ==0:
+#                 count+=1
+#         if count ==2:
+#             print(i)
+
+
+name = "Nandini"
+ans = ""
+for i in range(len(name)-1,-1,-1):
+    ans=ans+name[i]
+print(ans)
+
