60426374bdd2c111b84be84a3ce6bb8846eac307
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-13 12:27:54 +0530

    list pushed

---
+++ list.py
@@ -0,0 +1,18 @@
+# a = []
+# a.append(1)
+# a.append(2)
+# a.append(-10)
+# print(a)
+
+a = [1,1,2,3,2,1,2,1,1]
+# a.extend([1,2,3,4,5])
+# print(a)
+
+# for i in range(1,6):
+#     a.append(i)
+# print(a)
+# a.clear()
+# print(a)
+
+ans = a.count(1)
+print(ans)
