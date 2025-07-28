89999fe295e30fdd71683359942384bab885fd7e
UdayKumar1437 <udaykumarvalapudasu@gmail.com>
2025-07-28 06:44:17 +0530

    oops

---
+++ Oops.py
@@ -0,0 +1,32 @@
+# class Person:
+#     name = "Nandini"
+#     age = 20
+#     networth = "10 Crores"
+#     def info(self):
+#         print(f"{self.name} is {self.age} years old.")
+#     def getName(self):
+#         print(f"{self.name}")
+
+# aObj = Person()
+# bObj = Person()
+# cObj = Person()
+
+# bObj.name = "Uday Kumar"
+# bObj.age = 25
+
+# cObj.name = "Kedar"
+# cObj.age = 20
+
+# aObj.info()
+# bObj.getName()
+
+
+class Uday:
+    name = "Kumar"
+
+aObj = Uday()
+aObj.name = "Nandini"
+
+bObj = Uday()
+
+print(aObj.name)
